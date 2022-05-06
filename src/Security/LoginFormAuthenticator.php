b4411f5e3cb12366fc16e993e196ee309d61163f
gerardo montivero <gerardo.montivero@gmail.com>
2022-05-06 08:05:33 -0300

    Cap. 14 - Sistema de recordarme

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -14,6 +14,7 @@ use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
@@ -58,7 +59,8 @@ class LoginFormAuthenticator extends AbstractAuthenticator
                 new CsrfTokenBadge(
                     'authenticate',
                     $request->request->get('_csrf_token')
-                )
+                ),
+                new RememberMeBadge(),
             ]
         );
     }
