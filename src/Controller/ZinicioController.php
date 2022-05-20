37710a2969c56dc672cb9f55e396118c0f4c37c8
gerardo montivero <gerardo.montivero@gmail.com>
2022-05-20 08:18:36 -0300

    Cap. 21 - Negar el acceso en un controlador

--- src/Controller/ZinicioController.php
+++ src/Controller/ZinicioController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
 
 class ZinicioController extends AbstractController
 {
@@ -33,6 +35,18 @@ class ZinicioController extends AbstractController
      */
     public function inicio(): Response
     {
+        return $this->render('zinicio/index.html.twig', [
+            'controller_name' => 'ZinicioController',
+        ]);
+    }
+
+    /**
+     * @Route("/question", name="app_question")
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function question(): Response
+    {
+
         return $this->render('zinicio/index.html.twig', [
             'controller_name' => 'ZinicioController',
         ]);
