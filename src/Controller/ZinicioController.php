d15e63eb24a01d1885cfab291d2086484bfb4314
gerMDZ Gerardo Montivero <gerardo.montivero@gmail.com>
2022-06-27 07:49:30 -0300

    Cap. 24 - Obtención del objeto de usuario

--- src/Controller/ZinicioController.php
+++ src/Controller/ZinicioController.php
@@ -2,13 +2,14 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 
-class ZinicioController extends AbstractController
+class ZinicioController extends BaseController
 {
     /**
      * @Route("/api", name="zinicio")
@@ -47,6 +48,21 @@ class ZinicioController extends AbstractController
     public function question(): Response
     {
 
+        return $this->render('zinicio/index.html.twig', [
+            'controller_name' => 'ZinicioController',
+        ]);
+    }
+
+    /**
+     * @Route("/question", name="app_question")
+     * @IsGranted("IS_AUTHENTICATED_REMEMBERED")
+     */
+    public function answerVote(LoggerInterface $logger, ): Response
+    {
+        $logger->info('{user} is voting on answer {answer}', [
+            'user' => $this->getUser()->getEmail(),
+            'answer' => 'aquí una nota',
+        ]);
         return $this->render('zinicio/index.html.twig', [
             'controller_name' => 'ZinicioController',
         ]);
