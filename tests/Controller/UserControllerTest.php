0779c7831229a13a53f2c180d4402a29ab1e7d0c
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2022-09-11 10:11:29 -0300

    paso 1.3 dos

---
+++ tests/Controller/UserControllerTest.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace App\Test\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class UserControllerTest extends WebTestCase
+{
+    private KernelBrowser $client;
+    private UserRepository $repository;
+    private string $path = '/admin/user/';
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+        $this->repository = (static::getContainer()->get('doctrine'))->getRepository(User::class);
+
+        foreach ($this->repository->findAll() as $object) {
+            $this->repository->remove($object, true);
+        }
+    }
+
+    public function testIndex(): void
+    {
+        $crawler = $this->client->request('GET', $this->path);
+
+        self::assertResponseStatusCodeSame(200);
+        self::assertPageTitleContains('User index');
+
+        // Use the $crawler to perform additional assertions e.g.
+        // self::assertSame('Some text on the page', $crawler->filter('.p')->first());
+    }
+
+    public function testNew(): void
+    {
+        $originalNumObjectsInRepository = count($this->repository->findAll());
+
+        $this->markTestIncomplete();
+        $this->client->request('GET', sprintf('%snew', $this->path));
+
+        self::assertResponseStatusCodeSame(200);
+
+        $this->client->submitForm('Save', [
+            'user[email]' => 'Testing',
+            'user[roles]' => 'Testing',
+            'user[password]' => 'Testing',
+            'user[apiToken]' => 'Testing',
+            'user[FirstName]' => 'Testing',
+            'user[isVerified]' => 'Testing',
+            'user[isActive]' => 'Testing',
+            'user[organization]' => 'Testing',
+        ]);
+
+        self::assertResponseRedirects('/admin/user/');
+
+        self::assertSame($originalNumObjectsInRepository + 1, count($this->repository->findAll()));
+    }
+
+    public function testShow(): void
+    {
+        $this->markTestIncomplete();
+        $fixture = new User();
+        $fixture->setEmail('My Title');
+        $fixture->setRoles('My Title');
+        $fixture->setPassword('My Title');
+        $fixture->setApiToken('My Title');
+        $fixture->setFirstName('My Title');
+        $fixture->setIsVerified('My Title');
+        $fixture->setIsActive('My Title');
+        $fixture->setOrganization('My Title');
+
+        $this->repository->add($fixture, true);
+
+        $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
+
+        self::assertResponseStatusCodeSame(200);
+        self::assertPageTitleContains('User');
+
+        // Use assertions to check that the properties are properly displayed.
+    }
+
+    public function testEdit(): void
+    {
+        $this->markTestIncomplete();
+        $fixture = new User();
+        $fixture->setEmail('My Title');
+        $fixture->setRoles('My Title');
+        $fixture->setPassword('My Title');
+        $fixture->setApiToken('My Title');
+        $fixture->setFirstName('My Title');
+        $fixture->setIsVerified('My Title');
+        $fixture->setIsActive('My Title');
+        $fixture->setOrganization('My Title');
+
+        $this->repository->add($fixture, true);
+
+        $this->client->request('GET', sprintf('%s%s/edit', $this->path, $fixture->getId()));
+
+        $this->client->submitForm('Update', [
+            'user[email]' => 'Something New',
+            'user[roles]' => 'Something New',
+            'user[password]' => 'Something New',
+            'user[apiToken]' => 'Something New',
+            'user[FirstName]' => 'Something New',
+            'user[isVerified]' => 'Something New',
+            'user[isActive]' => 'Something New',
+            'user[organization]' => 'Something New',
+        ]);
+
+        self::assertResponseRedirects('/admin/user/');
+
+        $fixture = $this->repository->findAll();
+
+        self::assertSame('Something New', $fixture[0]->getEmail());
+        self::assertSame('Something New', $fixture[0]->getRoles());
+        self::assertSame('Something New', $fixture[0]->getPassword());
+        self::assertSame('Something New', $fixture[0]->getApiToken());
+        self::assertSame('Something New', $fixture[0]->getFirstName());
+        self::assertSame('Something New', $fixture[0]->getIsVerified());
+        self::assertSame('Something New', $fixture[0]->getIsActive());
+        self::assertSame('Something New', $fixture[0]->getOrganization());
+    }
+
+    public function testRemove(): void
+    {
+        $this->markTestIncomplete();
+
+        $originalNumObjectsInRepository = count($this->repository->findAll());
+
+        $fixture = new User();
+        $fixture->setEmail('My Title');
+        $fixture->setRoles('My Title');
+        $fixture->setPassword('My Title');
+        $fixture->setApiToken('My Title');
+        $fixture->setFirstName('My Title');
+        $fixture->setIsVerified('My Title');
+        $fixture->setIsActive('My Title');
+        $fixture->setOrganization('My Title');
+
+        $this->repository->add($fixture, true);
+
+        self::assertSame($originalNumObjectsInRepository + 1, count($this->repository->findAll()));
+
+        $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
+        $this->client->submitForm('Delete');
+
+        self::assertSame($originalNumObjectsInRepository, count($this->repository->findAll()));
+        self::assertResponseRedirects('/admin/user/');
+    }
+}
